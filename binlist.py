96b9eac94c8b44c4b6e760195ba84adf9b46e7d0
Coelodonta <45721842+Coelodonta@users.noreply.github.com>
2019-03-29 20:58:33 -0400

    Add bipolar, unipolar, hamming distance
    Added improved functionality including unipolar tp bipolar roundtrip and hamming distance.

--- binlist.py
+++ binlist.py
@@ -23,67 +23,91 @@
 #  
 
 class BinList():
-	def explode(self,number,fixedLength=0):
-		self.number=number
-		tobin = lambda n: n>0 and [n&1]+tobin(n>>1) or []
-		self.binlist=tobin(number)
-		self.binlist.reverse()
-		if len(self.binlist)<fixedLength:
-			for i in range(fixedLength-len(self.binlist)):
-				self.binlist.insert(0,0)
-		return self.binlist
+    def explode(self,number,fixedLength=0):
+        self.number=number
+        tobin = lambda n: n>0 and [n&1]+tobin(n>>1) or []
+        self.binlist=tobin(number)
+        self.binlist.reverse()
+        if len(self.binlist)<fixedLength:
+            for i in range(fixedLength-len(self.binlist)):
+                self.binlist.insert(0,0)
+        self.isbipolar=False
+        return self.binlist
 		
-	def implode(self,binlist):
-		self.binlist=binlist
-		self.number = int("".join(str(x) for x in binlist), 2)
-		return self.number
+    def implode(self,binlist):
+        self.binlist=binlist
+        self.number = int("".join(str(x) for x in binlist), 2)
+        self.isbipolar=False
+        return self.number
 
-	def hammingdistance(self,other):
-		if len(self.binlist) != len(other.binlist):
-			return None
-		return sum(s != o for s, o in zip(self.binlist, other.binlist))
-
+    def hammingdistance(self,other):
+        if len(self.binlist) != len(other.binlist):
+            return None
+        if self.isbipolar:
+            self.uniploar()
+        return sum(s != o for s, o in zip(self.binlist, other.binlist))
+    
+    def bipolar(self):
+        if not self.isbipolar:
+            bp=lambda x: -1 if (x<1) else 1
+            self.binlist=[bp(x) for x in self.binlist]
+            self.isbipolar=True
+        
+    def unipolar(self):
+        if self.bipolar:
+            up=lambda x: 0 if (x<1) else 1
+            self.binlist=[up(x) for x in self.binlist]
+            self.isbipolar=False
+        
 
 if __name__ == '__main__':
-	"""
+    """
 	Test code. The expected output is:
 	
-	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1]
-	[1, 0, 1, 1, 0, 1]
-	49
-	49
-	3
-	[1, 1, 0, 0, 0, 1]
-	[0, 1, 0, 1, 0, 1]
-	2
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1]
+        [1, 0, 1, 1, 0, 1]
+        49
+        49
+        3
+        [1, 1, 0, 0, 0, 1]
+        [0, 1, 0, 1, 0, 1]
+        2
+        [1, 1, -1, -1, -1, 1]
+        [1, 1, 0, 0, 0, 1]
+        21
 	
-	"""
+    """
 	
-	# Test fixed length list with padding
-	bl=BinList()
-	bl.explode(45,16)
-	print(bl.binlist)
-	# Test with smallest possible length 
-	bl.explode(45)
-	print(bl.binlist)
-	# Test imploding a list to a number
-	al=BinList()
-	al.implode([1,1,0,0,0,1])
-	print(al.number)
-	# Test with some extra padding
-	al.implode([0,0,0,0,0,1,1,0,0,0,1])
-	print(al.number)
-	# Test Hamming distance between two lists
-	x=BinList()
-	y=BinList()
-	x.implode([1,1,0,0,0,1])
-	y.implode([1,0,1,1,0,1])
-	print(x.hammingdistance(y))
-	# Test Hamming distance between a list and a number
-	y.explode(21,6)
-	print(x.binlist)
-	print(y.binlist)	
-	print(x.hammingdistance(y))
-
-	
-	
+    # Test fixed length list with padding
+    bl=BinList()
+    bl.explode(45,16)
+    print(bl.binlist)
+    # Test with smallest possible length 
+    bl.explode(45)
+    print(bl.binlist)
+    # Test imploding a list to a number
+    al=BinList()
+    al.implode([1,1,0,0,0,1])
+    print(al.number)
+    # Test with some extra padding
+    al.implode([0,0,0,0,0,1,1,0,0,0,1])
+    print(al.number)
+    # Test Hamming distance between two lists
+    x=BinList()
+    y=BinList()
+    x.implode([1,1,0,0,0,1])
+    y.implode([1,0,1,1,0,1])
+    print(x.hammingdistance(y))
+    # Test Hamming distance between a list and a number
+    y.explode(21,6)
+    print(x.binlist)
+    print(y.binlist)	
+    print(x.hammingdistance(y))
+    # Test bipolar conversion
+    x.bipolar()
+    print(x.binlist)
+    # Test unipolar conversion
+    x.unipolar()
+    print(x.binlist)
+    x.implode(y.binlist)
+    print(x.number)
